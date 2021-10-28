7e516b56626ca117150fcf145a1c21887d3ed0ab
dndanli <danielkcode@gmail.com>
2021-10-28 15:16:17 -0500

    moved nav styles into separate file

--- homepage/src/components/Navbar/Navbar.js
+++ homepage/src/components/Navbar/Navbar.js
@@ -15,44 +15,11 @@ import useNavStyleToggle from "./toggle";
 
 //styles
 import { containerStyle } from "./NavbarStyles";
-// import "./generalStyle.css";
+import { menuIconStyle } from "./NavbarStyles";
+import { navItemsStyle } from "./NavbarStyles";
+import { slideItemsStyle } from "./NavbarStyles";
+import { anchorStyle } from "./NavbarStyles";
 
-let menuIconStyle = {
-  right: "1em",
-  fontSize: "1.3em",
-  position: "absolute",
-  padding: "1em",
-  visibility: "",
-};
-
-let navItemsStyle = {
-  display: "flex",
-  justifyContent: "space-evenly",
-  width: "30%",
-  margin: 0,
-  padding: "1em",
-  visibility: "",
-};
-
-let slideItemsStyle = {
-  transition: "200ms ease",
-  background: "grey",
-  width: "20vw",
-  minHeight: "10vh",
-  margin: "1em",
-  padding: "1em",
-  position: "absolute",
-  top: "2em",
-  right: "1.6em",
-  borderRadius: ".5em",
-  zIndex: 1,
-  visibility: "",
-};
-
-let anchorStyle = {
-  color: "black",
-  textDecoration: "none",
-};
 const Navbar = () => {
   const matches = CheckQuery("(max-width: 753px)");
 
