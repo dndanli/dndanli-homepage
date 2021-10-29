d225003140b457daa578363250015537605c72c8
dndanli <danielkcode@gmail.com>
2021-10-29 16:28:26 -0500

    Projects page update

--- homepage/src/Pages/Projects.js
+++ homepage/src/Pages/Projects.js
@@ -1,7 +1,10 @@
 const Projects = () => {
   return (
     <div>
-      <h1>This is the projects page</h1>
+      <h1>Projects</h1>
+      <h4>Few of my projects I made while in college.</h4>
+
+      <h4>Latest:</h4>
     </div>
   );
 };
