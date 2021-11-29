052eebece52ebd3ac88827cf1269f7b2e7c96057
dndanli <danielkcode@gmail.com>
2021-11-28 22:46:23 -0600

    create global styles

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,12 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StyledNav } from "./Components/Layout/Navbar/Navbar.styles.js";
 import { StyledProjectPage } from "./Pages/Projects/MainProject.styles.js";
-
 import { StyledHomePage } from "./Pages/Home/Home.style.js";
-// import { MouseCursor } from "./Helpers/Cursor/Cursor.js";
-// import { MouseCursor } from "./Components/Effects/Cursor.jsx";
 import { StyledCursor } from "./Components/Effects/Cursor.style.js";
-import "./App.css";
 function App() {
   return (
     <BrowserRouter>
--- homepage/src/index.js
+++ homepage/src/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
 
+  body {
+    background-color: #000000;
+    background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+    margin: 0;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+  }
+`;
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
