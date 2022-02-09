5cbf4c7a41b27b400776b90157f70840d1febdb1
Daniel L <danielkcode@gmail.com>
2022-02-08 22:54:35 -0600

    updated slide nav bg color

--- homepage/src/Components/Layout/Navbar/Navbar.styles.js
+++ homepage/src/Components/Layout/Navbar/Navbar.styles.js
@@ -1,79 +1,77 @@
-import styled from 'styled-components'
-import Navbar from './Navbar'
-import { device } from '../../../Helpers/Devices';
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import { device } from "../../../Helpers/Devices";
 
 export const StyledNav = styled(Navbar)`
-    
+  display: flex;
+  justify-content: flex-end;
+  min-height: 5vh;
+
+  .menu__icon {
+    color: #cdd6d9;
+    position: absolute;
+    right: 1em;
+    top: 1em;
+    font-size: 1.3em;
+    display: none;
+  }
+
+  .navbar__nav {
+    margin: 0;
+    padding: 0em;
     display: flex;
-    justify-content: flex-end;
-    min-height: 5vh;
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    visibility: visible;
+  }
 
-    .menu__icon{
-        color:#cdd6d9;
-        position: absolute;
-        right: 1em;
-        top:1em;
-        font-size: 1.3em;
-        display: none;
-    };
+  .navbar__nav li {
+    margin: 1em;
+  }
 
-    .navbar__nav{
-        margin:0;
-        padding: 0em;
-        display: flex;
-        font-family: 'Poppins', sans-serif;
-        font-size: 1.5rem;
-        visibility: visible;
-    };
+  @media ${device.mobileL} {
+    .menu__icon {
+      display: block;
+    }
+    .navbar__nav {
+      visibility: hidden;
+    }
+  }
 
-    .navbar__nav li{
-        margin: 1em;
-    }
+  .slide__nav {
+    background-color: #c7d3dd;
+    font-family: Montserrat, sans-serif;
+    font-size: 1.1em;
+    padding: 1em;
+    position: absolute;
+    width: 6em;
+    min-height: 12vh;
+    top: 2em;
+    right: 1.6em;
+    border-radius: 0.5em;
+    transition: 200ms ease;
+    z-index: 1;
+  }
 
-    @media ${device.mobileL}{
-        .menu__icon{
-            display: block;
-        };
-        .navbar__nav{
-            visibility: hidden;
-        }
-    }
+  .slide__nav li {
+    margin: 0.5em;
+  }
 
-    
-    .slide__nav{
-        background-color: #353535;
-        font-family: Montserrat, sans-serif;
-        font-size: 1.1em;
-        padding: 1em;
-        position: absolute;
-        width: 6em;
-        min-height: 12vh;
-        top: 2em;
-        right: 1.6em;
-        border-radius: .5em;
-        transition: 200ms ease;
-        z-index:1
-    }
-    
-    .slide__nav li{
-        margin: .5em;
-    }
+  .inner__container a {
+    color: black;
+    text-decoration: none;
+  }
 
-    .inner__container a{
-        color:black;
-        text-decoration: none;
-    };
-
-    .inner__container li{
-        list-style-type: none;
+  .inner__container li {
+    list-style-type: none;
+  }
+  .nav__item a {
+    color: rgba(0, 0, 0, 0.8);
+  }
+  .nav__item {
+    &:hover a {
+      color: #e16259;
+      transition: 0.5s all ease-in-out;
     }
-    .nav__item a{
-        color: rgba(0,0,0,0.8);
-    }
-    .nav__item {    
-        &:hover a{
-            color: #e16259;
-            transition: 0.5s all ease-in-out;
-        }
-    }
+  }
 `;
