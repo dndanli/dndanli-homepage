4da8984c41b850ca6749938806ebe95738247db0
Daniel De Lima <danielkcode@gmail.com>
2022-03-02 02:14:01 -0600

    removed project

--- homepage/src/Pages/Projects/MainProject.jsx
+++ homepage/src/Pages/Projects/MainProject.jsx
@@ -1,38 +1,35 @@
 // import Card from "../../Components/Layout/Card/Card.jsx";
 import { StyledCard } from "../../Components/Layout/Card/Card.style";
 import "../../Fonts/fonts.css";
+import P1 from "../../Assets/Images/decide-my-meal.png";
+import P2 from "../../Assets/Images/expenses_tracker.png";
+import P3 from "../../Assets/Images/health-promotion.png";
 
-const MainProject = ({className}) => {
-  
+const MainProject = ({ className }) => {
   return (
     <div className={className}>
       <h1 className="headerStyle">Projects</h1>
       <div className="cards">
-
         <StyledCard
           projectName="Decide my meal"
-          projectDesc="A website to help people decide what to and where to eat"
-          projectURL="http://decidemymeal.com/">
-        </StyledCard>
+          projectDesc="A website to help people decide what to eat and where to eat"
+          projectURL="http://decidemymeal.com/"
+          projectScreenShot={P1}
+        ></StyledCard>
 
         <StyledCard
           projectName="Expenses tracker"
-          projectDesc="Expenses tracker web-app."
-          projectURL="https://github.com/dndanli/Expenses-tracker">
-        </StyledCard>
-
-        <StyledCard
-          projectName="Stock Scraper"
-          projectDesc="Financial stock data scraper."
-          projectURL="https://github.com/dndanli/StockScraper">
-        </StyledCard>
+          projectDesc="A web app to help you keep track of your expenses."
+          projectURL="https://github.com/dndanli/Expenses-tracker"
+          projectScreenShot={P2}
+        ></StyledCard>
 
         <StyledCard
           projectName="Health Promotion"
-          projectDesc="Web application to check user's Blood pressure levels and BMI index."
-          projectURL="https://github.com/tsorteberg/HealthPromotion">
-        </StyledCard>
-
+          projectDesc="A web app to check user's Blood pressure levels and BMI index."
+          projectURL="https://github.com/tsorteberg/HealthPromotion"
+          projectScreenShot={P3}
+        ></StyledCard>
       </div>
     </div>
   );
